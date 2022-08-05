a82c1ef82a1fbf91ac6af2b544dc1cb056f9f8a8
mysssssss <danila.2565@gmail.com>
2022-08-04 22:31:01 -0400

    intial author routes

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 
 
@@ -15,6 +17,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 
 const mongoose = require('mongoose')
@@ -26,4 +29,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000)
