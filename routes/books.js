8cfa2578248cbab2b56f8ff566c76c3311815dcc
mysssssss <danila.2565@gmail.com>
2022-08-07 21:17:12 -0400

    file upload database

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const fs = require('fs')
-const path = require('path')
 const Book = require('../models/book')
-const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
+
 
 
 //all books route
@@ -45,34 +36,27 @@ router.get('/new', async (req, res) => {
 })
 
 //create book route
-router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+router.post('/', async (req, res) => {
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     })
+saveCover(book, req.body.cover)
 
     try {
         const newBook = await book.save()
        // res.redirect(`books/${newBook.id}`)
         res.redirect('books')
     } catch {
-        if (book.coverImageName != null) {
-            removeBookCover(book.coverImageName)
-        }
         renderNewPage(res, book, true)
     }
 })
 
-function removeBookCover(fileName) {
-    fs.unlink(path.join(uploadPath, fileName), err => {
-        if (err) console.err(err)
-    })
-}
+
+
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -85,6 +69,15 @@ async function renderNewPage(res, book, hasError = false) {
         res.render('books/new', params)
     } catch {
         res.redirect('/books')
+    }
+}
+
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null &&  imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
     }
 }
 
